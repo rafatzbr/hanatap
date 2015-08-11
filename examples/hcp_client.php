dfed714745d2d7b47b4190b47df055d2966d4a4b
Rafael Zanetti <rafael.zanetti@sap.com>
2015-08-11 18:39:18 -0300

    Some Fixes, renaming and making autoload to work

--- examples/hcp_client.php
+++ examples/hcp_client.php
@@ -1,8 +1,14 @@
 <?php
-require_once '../library/HANATAP.php';
-require_once '../library/HANATC.php';
+// Autoload the classes
+require_once '../vendor/autoload.php';
+// You can also add it manually
+//require_once '../library/Request.php';
+//require_once '../library/Proxy.php';
+//require_once '../library/Client.php';
 
-$proxy = new HANATC(array(
+use HANATAP\Client;
+
+$proxy = new Client(array(
     'username'      => '', //Your HCP user. Ex, p000000000
     'accountname'   => '', // You HCP account name. Ex. p000000000trial
     'password'      => '', // Your HCP password
@@ -10,12 +16,10 @@ $proxy = new HANATC(array(
     'namespace'     => '', // Namespace of the application. Ex. app.package
     'proxy'         => '', // OPTIONAL: Proxy of your network. Ex. proxyhost:port
     // Extra options
-    'params'        => '$metadata',
-    'format'        => 'json'
+    'params'        => '$metadata'
 ));
 
 // Set the header to dislay the content correctly
 header('Content-Type: '.$proxy->getContentType());
 
-echo '<pre>';
-print_r(json_decode($proxy->getContents()));
+echo $proxy->getContents();
